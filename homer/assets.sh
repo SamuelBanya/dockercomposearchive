499b3951fb5dd2247457b0f9f05984821f588a02
Samuel Banya <sbanya@fastmail.com>
2026-03-03 23:30:02 -0600

    Adding wget script for Homer svg logos

---
+++ homer/assets.sh
@@ -0,0 +1,20 @@
+# !/bin/bash
+
+# These are just the 'wget' commands I used to grab the assets really quick:
+wget -O jellyfin.svg https://commons.wikimedia.org/wiki/File:Jellyfin_-_icon-transparent.svg
+
+wget -O navidrome.svg https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/navidrome.svg
+
+wget -O audiobookshelf.svg https://commons.wikimedia.org/wiki/File%3AAudiobookshelf_Logo.svg
+
+wget -O kavita.svg https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/kavita.svg
+
+wget -O immich.svg https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/immich.svg
+
+wget -O syncthing.svg https://commons.wikimedia.org/wiki/File:Syncthing_Logo.svg
+
+wget -O portainer.svg https://icon.icepanel.io/Technology/svg/Portainer.svg
+
+wget -O mealie.svg https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/svg/mealie.svg
+
+wget -O microbin.png https://cdn.jsdelivr.net/gh/homarr-labs/dashboard-icons/png/microbin.png
