482972fe9f83f1fb5d3c19f6dc273ec8af30c291
Samuel Banya <sbanya@fastmail.com>
2026-03-19 01:08:19 -0500

    Adjusting Zim file output directory to be placed into Downloads folder

--- kiwix/deployZimFiles.sh
+++ kiwix/deployZimFiles.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Arch wiki:
-wget -O archlinux_en_all_maxi_2025-09.zim https://download.kiwix.org/zim/other/archlinux_en_all_maxi_2025-09.zim
+wget -O ~/Downloads/archlinux_en_all_maxi_2025-09.zim https://download.kiwix.org/zim/other/archlinux_en_all_maxi_2025-09.zim
 
 # Stardew Valley wiki:
-wget -O stardewvalleywiki_en_all_2026-01.zim https://download.kiwix.org/zim/other/stardewvalleywiki_en_all_2026-01.zim
+wget -O ~/Downloads/stardewvalleywiki_en_all_2026-01.zim https://download.kiwix.org/zim/other/stardewvalleywiki_en_all_2026-01.zim
 
 # Terraria wiki:
-wget -O terrariawiki_en_2025-10.zim https://mirror.download.kiwix.org/zim/.hidden/dev/terrariawiki_en_2025-10.zim
+wget -O ~/Downloads/terrariawiki_en_2025-10.zim https://mirror.download.kiwix.org/zim/.hidden/dev/terrariawiki_en_2025-10.zim
