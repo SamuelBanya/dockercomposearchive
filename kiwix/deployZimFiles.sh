d3ff5a2125de1cc06b652f171d883a02a8a685a3
Samuel Banya <sbanya@fastmail.com>
2026-03-19 00:56:16 -0500

    Adding changes to include Terraria as well

--- kiwix/deployZimFiles.sh
+++ kiwix/deployZimFiles.sh
@@ -8,3 +8,6 @@ wget -O /home/sam/hoard/ZimFiles/stardewvalleywiki_en_all_2026-01.zim https://do
 
 # Minecraft wiki:
 wget -O /home/sam/hoard/ZimFiles/minecraftwiki_en_all_maxi_2026-02 https://download.kiwix.org/zim/other/minecraftwiki_en_all_maxi_2026-02.zim
+
+# Terraria wiki:
+wget -O /home/sam/hoard/ZimFiles/terrariawiki_en_2025-10.zim https://mirror.download.kiwix.org/zim/.hidden/dev/terrariawiki_en_2025-10.zim
