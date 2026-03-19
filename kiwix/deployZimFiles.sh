cc086d1f863dfc8dd769c9eb8e63586b9215b6cf
Samuel Banya <sbanya@fastmail.com>
2026-03-19 00:37:43 -0500

    Adding only modern Zim files for web UI version, and adding deployment script

---
+++ kiwix/deployZimFiles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Arch wiki:
+wget -O /home/sam/hoard/ZimFiles/archlinux_en_all_maxi_2025-09.zim https://download.kiwix.org/zim/other/archlinux_en_all_maxi_2025-09.zim
+
+# Stardew Valley wiki:
+wget -O /home/sam/hoard/ZimFiles/stardewvalleywiki_en_all_2026-01.zim https://download.kiwix.org/zim/other/stardewvalleywiki_en_all_2026-01.zim
+
+# Minecraft wiki:
+wget -O /home/sam/hoard/ZimFiles/minecraftwiki_en_all_maxi_2026-02 https://download.kiwix.org/zim/other/minecraftwiki_en_all_maxi_2026-02.zim
