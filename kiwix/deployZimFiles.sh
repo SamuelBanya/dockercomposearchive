d1482f01d0dec641b0cdab5a69005499dbd61647
Samuel Banya <sbanya@fastmail.com>
2026-03-19 01:04:30 -0500

    Removing Minecraft since the Zim file is too big imo

--- kiwix/deployZimFiles.sh
+++ kiwix/deployZimFiles.sh
@@ -6,8 +6,5 @@ wget -O /home/sam/hoard/ZimFiles/archlinux_en_all_maxi_2025-09.zim https://downl
 # Stardew Valley wiki:
 wget -O /home/sam/hoard/ZimFiles/stardewvalleywiki_en_all_2026-01.zim https://download.kiwix.org/zim/other/stardewvalleywiki_en_all_2026-01.zim
 
-# Minecraft wiki:
-wget -O /home/sam/hoard/ZimFiles/minecraftwiki_en_all_maxi_2026-02 https://download.kiwix.org/zim/other/minecraftwiki_en_all_maxi_2026-02.zim
-
 # Terraria wiki:
 wget -O /home/sam/hoard/ZimFiles/terrariawiki_en_2025-10.zim https://mirror.download.kiwix.org/zim/.hidden/dev/terrariawiki_en_2025-10.zim
