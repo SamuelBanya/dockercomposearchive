15a7a29d2d9b31d7334d8916becffa903bb40ef4
Samuel Banya <sbanya@fastmail.com>
2026-03-19 00:45:52 -0500

    Adding changes

--- kiwix/deployZimFiles.sh
+++ kiwix/deployZimFiles.sh
@@ -7,4 +7,4 @@ wget -O /home/sam/hoard/ZimFiles/archlinux_en_all_maxi_2025-09.zim https://downl
 wget -O /home/sam/hoard/ZimFiles/stardewvalleywiki_en_all_2026-01.zim https://download.kiwix.org/zim/other/stardewvalleywiki_en_all_2026-01.zim
 
 # Minecraft wiki:
-wget -O /home/sam/hoard/ZimFiles/minecraftwiki_en_all_maxi_2026-02 https://download.kiwix.org/zim/other/minecraftwiki_en_all_maxi_2026-02.zim
+wget -O /home/sam/hoard/ZimFiles/minecraftwiki_en_all_maxi_2026-02 https://download.kiwix.org/zim/other/minecraftwiki_en_all_maxi_2026-02.zimT
